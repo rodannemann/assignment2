8e7eded01a7c873f2bae7604ccdbb06cb13699b8
Rodrigo <rodannemann@gmail.com>
2014-06-14 20:21:57 -0300

    functional code

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,17 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    i <- NULL
+    set <- function(y) {
+        x <<- y
+        i <<- NULL
+    }
+    get <- function() x
+    setinv <- function(inv) i <<- inv
+    getinv <- function() i
+    list(set = set, get = get,
+         setinv = setinv,
+         getinv = getinv)
 }
 
 
@@ -12,4 +22,23 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+    out <- tryCatch(
+    {
+        i <- x$getinv()
+        if(!is.null(i)) {
+            message("getting cached data")
+            return(i)
+        }
+        data <- x$get()
+        i <- solve(data, ...)    
+        x$setinv(i)
+        i
+    },
+    error=function(cond) {
+        message(paste("Input matrix is singular (or not invertible)...", url))
+        #message("Here's the original error message:")
+        #message(cond)
+        return(NA)
+    }
+    )
 }
